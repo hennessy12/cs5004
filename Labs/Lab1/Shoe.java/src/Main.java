2ecfaa98c19db38b22e422f494b461c6f75986c9
hennessy12 <xuqi22alianqiu@gmail.com>
2025-01-22 17:35:42 -0800

    To use the constructor

--- Labs/Lab1/Shoe.java/src/Main.java
+++ Labs/Lab1/Shoe.java/src/Main.java
@@ -1,78 +1,15 @@
-/**
- * The Shoe class represents a shoe with attributes such as type, color, brand, and size.
- * This class provides a constructor for initializing the attributes, getter methods to retrieve
- * attribute values, and a toString method to summarize the shoe details.
- */
-public class Shoe {
+public class Main {
+  public static void main(String[] args) {
+    // Create a Shoe object
+    Shoe myShoe = new Shoe("Sneaker", "Black", "Nike", 10.5);
 
-  private String type;
-  private String color;
-  private String brand;
-  private double size;
+    // Use the getter methods
+    System.out.println("Type: " + myShoe.getType());
+    System.out.println("Color: " + myShoe.getColor());
+    System.out.println("Brand: " + myShoe.getBrand());
+    System.out.println("Size: " + myShoe.getSize());
 
-  /**
-   * Constructs a new Shoe object with the specified type, color, brand, and size.
-   *
-   * @param type  the type of shoe (e.g., sneaker, boot, sandal)
-   * @param color the color of the shoe (e.g., black, white, red)
-   * @param brand the brand of the shoe (e.g., Nike, Adidas)
-   * @param size  the size of the shoe (e.g., 9.5, 10)
-   */
-  public Shoe(String type, String color, String brand, double size) {
-    this.type = type;
-    this.color = color;
-    this.brand = brand;
-    this.size = size;
-  }
-
-  /**
-   * Returns the type of the shoe.
-   *
-   * @return the type of the shoe
-   */
-  public String getType() {
-    return type;
-  }
-
-  /**
-   * Returns the color of the shoe.
-   *
-   * @return the color of the shoe
-   */
-  public String getColor() {
-    return color;
-  }
-
-  /**
-   * Returns the brand of the shoe.
-   *
-   * @return the brand of the shoe
-   */
-  public String getBrand() {
-    return brand;
-  }
-
-  /**
-   * Returns the size of the shoe.
-   *
-   * @return the size of the shoe
-   */
-  public double getSize() {
-    return size;
-  }
-
-  /**
-   * Returns a string representation of the shoe, summarizing its attributes.
-   *
-   * @return a string containing the type, color, brand, and size of the shoe
-   */
-  @Override
-  public String toString() {
-    return "Shoe{" +
-        "type='" + type + '\'' +
-        ", color='" + color + '\'' +
-        ", brand='" + brand + '\'' +
-        ", size=" + size +
-        '}';
+    // Print the Shoe object using toString()
+    System.out.println(myShoe);
   }
 }
